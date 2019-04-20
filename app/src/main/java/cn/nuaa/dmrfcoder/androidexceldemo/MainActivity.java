be2bc54d9e344278d741175b59a613f27a62dfd8
DmrfCoder <xuefanggang97@gmail.com>
2019-04-20 15:17:55 +0800

    update minSdkVersion to 23

--- app/src/main/java/cn/nuaa/dmrfcoder/androidexceldemo/MainActivity.java
+++ app/src/main/java/cn/nuaa/dmrfcoder/androidexceldemo/MainActivity.java
@@ -6,30 +6,25 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
-import android.os.Environment;
+import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import cn.nuaa.dmrfcoder.androidexceldemo.Bean.DemoBean;
 import cn.nuaa.dmrfcoder.androidexceldemo.Utils.ExcelUtil;
-import cn.nuaa.dmrfcoder.androidexceldemo.Utils.PermissionUtil;
 
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
