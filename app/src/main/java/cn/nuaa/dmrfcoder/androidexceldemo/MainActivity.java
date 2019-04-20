bf0739bb0a2dce794aa90b318394813bca0f1ffb
DmrfCoder <xuefanggang97@gmail.com>
2019-04-20 17:36:39 +0800

    add open dir button

--- app/src/main/java/cn/nuaa/dmrfcoder/androidexceldemo/MainActivity.java
+++ app/src/main/java/cn/nuaa/dmrfcoder/androidexceldemo/MainActivity.java
@@ -6,14 +6,17 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.os.Environment;
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -26,39 +29,58 @@ import java.util.List;
 import cn.nuaa.dmrfcoder.androidexceldemo.Bean.DemoBean;
 import cn.nuaa.dmrfcoder.androidexceldemo.Utils.ExcelUtil;
 
-import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private Button exportButton;
+    private Button openButton;
     private TextView textView;
 
     private AlertDialog alertDialog;
     private AlertDialog mDialog;
 
 
-    private static final int NOT_NOTICE = 2;//如果勾选了不再询问
-
-
     String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+
+    private int REQUEST_PERMISSION_CODE = 1000;
+
+
+    private String filePath = Environment.getExternalStorageDirectory() + "/AndroidExcelDemo";
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT > 23) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this,
+                    permissions[0])
+                    == PackageManager.PERMISSION_GRANTED) {
+                //授予权限
+                Log.i("requestPermission:", "用户之前已经授予了权限！");
+            } else {
+                //未获得权限
+                Log.i("requestPermission:", "未获得权限，现在申请！");
+                requestPermissions(permissions
+                        , REQUEST_PERMISSION_CODE);
+            }
+        }
+
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestPermission();
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+        builder.detectFileUriExposure();
 
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PERMISSION_GRANTED) {
-            Toast.makeText(this, "您还没申请权限!", Toast.LENGTH_SHORT).show();
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-        } else {
-            Toast.makeText(this, "您已经申请了权限!", Toast.LENGTH_SHORT).show();
-        }
-
-
-        exportButton = findViewById(R.id.button);
+        exportButton = findViewById(R.id.export_button);
         exportButton.setOnClickListener(this);
+
+        openButton = findViewById(R.id.open_button);
+        openButton.setOnClickListener(this);
         textView = findViewById(R.id.textView);
 
 
@@ -68,53 +90,33 @@ public class MainActivity extends Activity implements View.OnClickListener {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if (requestCode == 1) {
-            for (int i = 0; i < permissions.length; i++) {
-                if (grantResults[i] == PERMISSION_GRANTED) {//选择了“始终允许”
-                    Toast.makeText(this, "" + "权限" + permissions[i] + "申请成功", Toast.LENGTH_SHORT).show();
-                } else {
-                    if (!ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[i])) {//用户选择了禁止不再询问
+        if (requestCode == REQUEST_PERMISSION_CODE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.i("onPermissionsResult:", "权限" + permissions[0] + "申请成功");
+                // permission was granted, yay! Do the
+                // contacts-related task you need to do.
 
-                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                        builder.setTitle("permission")
-                                .setMessage("点击允许才可以使用我们的app哦")
-                                .setPositiveButton("去允许", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int id) {
-                                        if (mDialog != null && mDialog.isShowing()) {
-                                            mDialog.dismiss();
-                                        }
-                                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                                        Uri uri = Uri.fromParts("package", getPackageName(), null);//注意就是"package",不用改成自己的包名
-                                        intent.setData(uri);
-                                        startActivityForResult(intent, NOT_NOTICE);
-                                    }
-                                });
-                        mDialog = builder.create();
-                        mDialog.setCanceledOnTouchOutside(false);
-                        mDialog.show();
-
-
-                    } else {//选择禁止
-                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                        builder.setTitle("permission")
-                                .setMessage("点击允许才可以使用我们的app哦")
-                                .setPositiveButton("去允许", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int id) {
-                                        if (alertDialog != null && alertDialog.isShowing()) {
-                                            alertDialog.dismiss();
-                                        }
-                                        ActivityCompat.requestPermissions(MainActivity.this,
-                                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-                                    }
-                                });
-                        alertDialog = builder.create();
-                        alertDialog.setCanceledOnTouchOutside(false);
-                        alertDialog.show();
-                    }
-
-                }
+            } else {
+                Log.i("onPermissionsResult:", "用户拒绝了权限申请");
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("permission")
+                        .setMessage("点击允许才可以使用我们的app哦")
+                        .setPositiveButton("去允许", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                if (alertDialog != null && alertDialog.isShowing()) {
+                                    alertDialog.dismiss();
+                                }
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                            }
+                        });
+                alertDialog = builder.create();
+                alertDialog.setCanceledOnTouchOutside(false);
+                alertDialog.show();
+                // permission denied, boo! Disable the
+                // functionality that depends on this permission.
             }
         }
     }
@@ -126,18 +128,33 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.button:
+            case R.id.export_button:
                 exportExcel(this);
+
                 break;
+            case R.id.open_button:
+                openDir();
             default:
                 break;
         }
     }
 
+    private void openDir() {
+
+        File file = new File(filePath);
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setDataAndType(Uri.fromFile(file), "file/*");
+        try {
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(this, "没有正确打开文件管理器", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
     private void exportExcel(Context context) {
-
-
-        String filePath =  "/AndroidExcelDemo";
 
 
         File file = new File(filePath);
